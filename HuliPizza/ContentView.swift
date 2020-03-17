40cfb026ddf12a3c854e733a272a3569dc1a5641
Chase McElroy <ichasem@gmail.com>
2020-03-16 17:33:09 -0700

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Chase McElroy on 3/16/20.
+//  Copyright © 2020 Anivive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
